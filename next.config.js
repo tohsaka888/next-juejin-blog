0b62416822c583784de0abe1ab7f0981edd39e29
tohsaka888 <156132264@qq.com>
2022-06-03 15:50:37 +0800

    feat: enable mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
+const withMDX = require('@next/mdx')(
+  {
+    extension: /\.mdx?$/,
+    options: {
+      providerImportSource: '@mdx-js/react',
+      remarkPlugins: [],
+      rehypePlugins: [],
+    }
+  }
+)
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withMDX({
+  ...nextConfig,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})

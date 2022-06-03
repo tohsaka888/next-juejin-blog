0a78d38f32b2d0cabbd6a9468b5224031fbb6ac9
tohsaka888 <156132264@qq.com>
2022-06-03 12:09:05 +0800

    feat: menu config

---
+++ components/Header/menuConfig.ts
@@ -0,0 +1,32 @@
+import { MenuItemProps } from "../../config/type";
+
+export const items: MenuItemProps[] = [
+  {
+    label: "首页",
+    key: "homepage",
+  },
+  {
+    label: "前端",
+    key: "frontend",
+  },
+  {
+    label: "后端",
+    key: "backend",
+  },
+  {
+    label: "Android",
+    key: "android",
+  },
+  {
+    label: "iOS",
+    key: "ios",
+  },
+  {
+    label: "人工智能",
+    key: "ai",
+  },
+  {
+    label: "算法",
+    key: "algorithm",
+  },
+];

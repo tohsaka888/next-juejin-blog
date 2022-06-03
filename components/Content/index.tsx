5f188c56ab3af89547f735e49f5938032eec8717
tohsaka888 <156132264@qq.com>
2022-06-03 18:16:38 +0800

    feat: add menu

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,17 +1,43 @@
 import React from "react";
-import { Box, useColorMode } from "@chakra-ui/react";
+import { Box, useColorMode, Divider } from "@chakra-ui/react";
 import { shadows } from "../../config/theme";
+import ArticleCard from "components/ArticleCard";
+import AnimatedMenu from "react-spring-menu";
+import { MenuItemProps } from "config/type";
+
+const items: MenuItemProps[] = [
+  {
+    label: "推荐",
+    key: "recommend",
+  },
+  {
+    label: "最新",
+    key: "latest",
+  },
+  {
+    label: "热门",
+    key: "hot",
+  },
+];
 
 function Content() {
   const { colorMode } = useColorMode();
   return (
     <Box
-      height={"100vh"}
       flex={3}
       bg={colorMode === "light" ? "#fff" : undefined}
       shadow={shadows[colorMode]}
-      borderRadius={"8px"}
-    ></Box>
+      pos={"relative"}
+      // borderRadius={"8px"}
+    >
+      <AnimatedMenu
+        items={items}
+        defaultSelectedKeys={["recommend"]}
+        itemStyle={{ fontSize: "14px" }}
+      />
+      <Divider />
+      <ArticleCard />
+    </Box>
   );
 }
 

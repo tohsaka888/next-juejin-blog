953856ad7297ae33554a9d7c2124994f80ec7ef8
tohsaka888 <156132264@qq.com>
2022-06-03 12:50:17 +0800

    feat: global shadow

--- config/theme.ts
+++ config/theme.ts
@@ -1,15 +1,21 @@
 // theme.ts
 
 // 1. import `extendTheme` function
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 // 2. Add your color mode config
 const config: ThemeConfig = {
-  initialColorMode: 'light',
+  initialColorMode: "light",
   useSystemColorMode: true,
-}
+};
+
+const shadows = {
+  light: "0px 0px 8px rgba(0, 0, 0, 0.1)",
+  dark: "0px 0px 10px #1890ff",
+};
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config });
 
-export default theme
+export default theme;
+export { shadows };

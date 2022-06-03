ed5ef7e7dc0a93481b96569fe266734de5beafd2
tohsaka888 <156132264@qq.com>
2022-06-03 15:52:07 +0800

    feat: every page show header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { MDXProvider } from "@mdx-js/react";
+import Header from "components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <MDXProvider>
+        <Header />
+        <Component {...pageProps} />
+      </MDXProvider>
     </ChakraProvider>
   );
 }

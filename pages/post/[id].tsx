84cf68e025f2a0b9ee8527394299f2578da1ce61
tohsaka888 <156132264@qq.com>
2022-06-03 15:52:32 +0800

    feat: article detail page

---
+++ pages/post/[id].tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import { useRouter } from "next/router";
+import React from "react";
+
+const ArticleDetail: NextPage<{
+  source: MDXRemoteSerializeResult<Record<string, unknown>>;
+}> = ({ source }) => {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <div>
+      {id}
+      <MDXRemote {...source} />
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const source = "# h1 ## h2";
+  const mdxSource = await serialize(source);
+  return { props: { source: mdxSource } };
+}
+
+export default ArticleDetail;

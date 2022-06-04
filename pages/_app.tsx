b388b238599bb116fdc620dd2821e106eee7d13d
tohsaka888 <156132264@qq.com>
2022-06-04 15:53:17 +0800

    feat: use mdx format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,22 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { MDXProvider } from "@mdx-js/react";
 import Header from "components/Header";
+import { H1, H2, H3, H4, H5, H6 } from "markdown/Heading";
+import "../styles/prism-theme.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <MDXProvider>
+      <MDXProvider
+        components={{
+          h1: H1,
+          h2: H2,
+          h3: H3,
+          h4: H4,
+          h5: H5,
+          h6: H6,
+        }}
+      >
         <Header />
         <Component {...pageProps} />
       </MDXProvider>

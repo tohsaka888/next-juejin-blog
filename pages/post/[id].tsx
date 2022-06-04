485479ffb64b8c0e1eaa9a835c9203754b9b9220
tohsaka888 <156132264@qq.com>
2022-06-04 17:04:46 +0800

    feat: add some plugins

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -4,10 +4,18 @@ import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import React from "react";
 import { InfoResponse } from "../../config/type";
-import { Box, useColorMode } from "@chakra-ui/react";
-import highlight from "rehype-highlight";
+import { Box, useColorMode, OrderedList } from "@chakra-ui/react";
 import rehypePrism from "rehype-prism-plus";
 import { H1, H2, H3, H4, H5, H6 } from "markdown/Heading";
+import Paragraph from "markdown/Paragraph";
+import { OrderListItem } from "markdown/OrderList";
+import codesandbox from "remark-codesandbox";
+import embedIamge from "remark-embed-images";
+import extendedTable from "remark-extended-table";
+import reamrkTypescript from "remark-typescript";
+import directive from "remark-directive";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import remarkGfm from "remark-gfm";
 
 const ArticleDetail: NextPage<{
   source: MDXRemoteSerializeResult<Record<string, unknown>>;
@@ -29,6 +37,9 @@ const ArticleDetail: NextPage<{
           h4: H4,
           h5: H5,
           h6: H6,
+          p: Paragraph,
+          ol: OrderedList,
+          li: OrderListItem,
         }}
       />
     </Box>
@@ -40,8 +51,18 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const data: InfoResponse = await res.json();
   const mdxSource = await serialize(data.info.content, {
     mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [highlight, rehypePrism],
+      remarkPlugins: [
+        [codesandbox, { mode: "button" }],
+        embedIamge,
+        extendedTable,
+        reamrkTypescript,
+        directive,
+        remarkGfm,
+      ],
+      rehypePlugins: [
+        rehypePrism,
+        rehypeAutolinkHeadings,
+      ],
     },
   });
   return { props: { source: mdxSource } };

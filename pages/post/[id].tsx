c8359dec6ae1172dba1deb299a7c9eaf8300a5d8
tohsaka888 <156132264@qq.com>
2022-06-04 15:53:39 +0800

    feat: mdx page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,27 +1,41 @@
-import { NextPage } from "next";
+import { baseUrl } from "config/baseUrl";
+import { GetServerSideProps, NextPage } from "next";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import { useRouter } from "next/router";
 import React from "react";
+import { InfoResponse } from "../../config/type";
+import { Box, useColorMode } from "@chakra-ui/react";
+import highlight from "rehype-highlight";
+import rehypePrism from "rehype-prism-plus";
 
 const ArticleDetail: NextPage<{
   source: MDXRemoteSerializeResult<Record<string, unknown>>;
 }> = ({ source }) => {
   const router = useRouter();
-  const { id } = router.query;
+  const { colorMode } = useColorMode();
   return (
-    <div>
-      {id}
+    <Box
+      width="50vw"
+      margin={"auto"}
+      mt={"80px"}
+      bg={colorMode === "light" ? "#fff" : undefined}
+    >
       <MDXRemote {...source} />
-    </div>
+    </Box>
   );
 };
 
-export async function getServerSideProps() {
-  const source = `# h1 
-   ## h2`;
-  const mdxSource = await serialize(source);
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const res = await fetch(`${baseUrl}/api/post/${query.id}`);
+  const data: InfoResponse = await res.json();
+  const mdxSource = await serialize(data.info.content, {
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [highlight, rehypePrism],
+    },
+  });
   return { props: { source: mdxSource } };
-}
+};
 
 export default ArticleDetail;

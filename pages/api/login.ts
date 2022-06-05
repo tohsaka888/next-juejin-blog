0e8afc12ec9f6c6e9d23073c90995e36a7f16f3b
tohsaka888 <156132264@qq.com>
2022-06-06 06:43:25 +0800

    feat: 邮箱登录

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from 'jsonwebtoken'
 import { LoginResponse } from "config/type";
-import mailconfig from "lib/mailconfig";
+import connectDB from "lib/connectDb";
 
 // t=0 密码登录 t=1 验证码登录(数据库无账号自动注册)
 export default async function handler(
@@ -14,19 +14,21 @@ export default async function handler(
     const token = jwt.sign(body, 'shhhhh');
     res.status(200).json({ success: true, token: token });
   } else {
-    // const code = String(Math.floor(Math.random() * 1000000)).padEnd(6, '0') //生成6位随机验证码
-    // const mailAddress = body.username
-    // const mail = {
-    //   from: `"tohsaka888"<tohsaka888@qq.com>`,// 发件人
-    //   subject: '掘金登录验证码',//邮箱主题
-    //   to: mailAddress,//收件人，这里由post请求传递过来
-    //   // 邮件内容，用html格式编写
-    //   html: `
-    //       <p>您好！</p>
-    //       <p>您的验证码是：<strong style="color:orangered;">${code}</strong></p>
-    //       <p>如果不是您本人操作，请无视此邮件</p>
-    //   `
-    // };
-    // await mailconfig.sendMail(mail)
+    const db = await connectDB();
+    const collection = db.collection("AuthCode")
+    const userCollection = db.collection("user")
+    const authcode = await collection.findOne({ email: body.email })
+    const user = await userCollection.findOne({ email: body.email })
+
+    if (authcode.code === body.code) {
+      const token = jwt.sign({ email: body.email }, 'shhhhh');
+      res.send({ success: true, token: token, needRegister: !user });
+    } else {
+      res.send({ success: false, msg: '验证码错误', token: '' })
+    }
+
+    setTimeout(() => {
+      collection.deleteOne({ email: body.email })
+    }, 5 * 60 * 1000)
   }
 }

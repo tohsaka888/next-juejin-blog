7ca20b2bdbd9266e81bd9d06aee82453ce7b4ec3
tohsaka888 <156132264@qq.com>
2022-06-06 06:58:16 +0800

    feat: 捕获错误信息

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -9,26 +9,30 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<LoginResponse>
 ) {
-  const body = JSON.parse(req.body || {})
-  if (body.t === 0 || !body.t) {
-    const token = jwt.sign(body, 'shhhhh');
-    res.status(200).json({ success: true, token: token });
-  } else {
-    const db = await connectDB();
-    const collection = db.collection("AuthCode")
-    const userCollection = db.collection("user")
-    const authcode = await collection.findOne({ email: body.email })
-    const user = await userCollection.findOne({ email: body.email })
-
-    if (authcode.code === body.code) {
-      const token = jwt.sign({ email: body.email }, 'shhhhh');
-      res.send({ success: true, token: token, needRegister: !user });
+  try {
+    const body = JSON.parse(req.body || {})
+    if (body.t === 0 || !body.t) {
+      const token = jwt.sign(body, 'shhhhh');
+      res.status(200).json({ success: true, token: token });
     } else {
-      res.send({ success: false, msg: '验证码错误', token: '' })
-    }
+      const db = await connectDB();
+      const collection = db.collection("AuthCode")
+      const userCollection = db.collection("user")
+      const authcode = await collection.findOne({ email: body.email })
+      const user = await userCollection.findOne({ email: body.email })
 
-    setTimeout(() => {
-      collection.deleteOne({ email: body.email })
-    }, 5 * 60 * 1000)
+      if (authcode.code === body.code) {
+        const token = jwt.sign({ email: body.email }, 'shhhhh');
+        res.send({ success: true, token: token, needRegister: !user });
+      } else {
+        res.send({ success: false, msg: '验证码错误', token: '' })
+      }
+
+      setTimeout(() => {
+        collection.deleteOne({ email: body.email })
+      }, 5 * 60 * 1000)
+    }
+  } catch (error: any) {
+    res.status(404).send({ success: false, msg: error.message, token: '' })
   }
 }

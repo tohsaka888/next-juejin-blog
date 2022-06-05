164c6cba5522da5b1fc7d4c89ee0486887f78c43
tohsaka888 <156132264@qq.com>
2022-06-06 05:48:52 +0800

    feat: 发送右键

---
+++ pages/api/sendmail.ts
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import jwt from 'jsonwebtoken'
+import { LoginResponse, MailResponse } from "config/type";
+import mailconfig from "lib/mailconfig";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<MailResponse>
+) {
+  const body = JSON.parse(req.body || {})
+
+  const code = String(Math.floor(Math.random() * 1000000)).padEnd(6, '0') //生成6位随机验证码
+  const mailAddress = body.email
+  const mail = {
+    from: `"tohsaka888"<tohsaka888@qq.com>`,// 发件人
+    subject: '掘金登录验证码',//邮箱主题
+    to: mailAddress,//收件人，这里由post请求传递过来
+    // 邮件内容，用html格式编写
+    html: `
+          <p>您好！</p>
+          <p>您的验证码是：<strong style="color:orangered;">${code}</strong></p>
+          <p>如果不是您本人操作，请无视此邮件</p>
+      `
+  };
+  await mailconfig.sendMail(mail)
+  res.status(200).send({ success: true })
+}

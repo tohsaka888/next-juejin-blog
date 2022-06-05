323bed2e1f2cc13bc663d3a54beab49f29832402
tohsaka888 <156132264@qq.com>
2022-06-05 15:30:33 +0800

    style: 移除不必要的css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider, useDisclosure } from '@chakra-ui/react';
 import Header from "components/Header";
-import "../styles/prism-theme.css";
 import LoginModal from '../components/LoginModal/index';
 
 function MyApp({ Component, pageProps }: AppProps) {

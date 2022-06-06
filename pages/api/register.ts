596c8a135c2cc150347e70a03c01566a3b910561
tohsaka888 <156132264@qq.com>
2022-06-06 08:58:24 +0800

    feat: 注册接口

---
+++ pages/api/register.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectDB from "lib/connectDb";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { RegisterResponse } from "../../config/type";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<RegisterResponse>
+) {
+  const body = JSON.parse(req.body || {});
+  const db = await connectDB();
+  const collection = db.collection("user");
+  collection.updateOne({ token: body.token }, { $set: { username: body.username, password: body.password } })
+  res.status(200).json({ success: true, msg: '注册成功' });
+}

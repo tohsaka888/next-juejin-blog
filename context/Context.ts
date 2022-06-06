e76a01b9694ec0b9f764b4062b0eaea5162a6dc2
tohsaka888 <156132264@qq.com>
2022-06-06 08:57:35 +0800

    feat: 增加登录context

--- context/Context.ts
+++ context/Context.ts
@@ -2,3 +2,4 @@ import { createContext } from "react";
 import { ArticleBriefInfo } from "../config/type";
 
 export const ListContext = createContext<ArticleBriefInfo[] | null>(null);
+export const LoginStatusContext = createContext<any | null>(null);

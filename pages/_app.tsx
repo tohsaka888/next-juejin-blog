a3fd58edc42ec1fcb688bd6e91bee9e14c756e3a
tohsaka888 <156132264@qq.com>
2022-06-06 08:57:55 +0800

    feat: 初始获取登陆状态

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,49 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, useDisclosure } from '@chakra-ui/react';
 import Header from "components/Header";
 import LoginModal from '../components/LoginModal/index';
+import { LoginStatusContext } from "context/Context";
+import { useEffect, useState } from "react";
+import { LoginStatus, LoginStatusResponse } from "config/type";
+import { baseUrl } from "config/baseUrl";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const props = useDisclosure()
+  const [loginStatus, setLoginStatus] = useState<LoginStatus>({
+    status: false,
+    username: "",
+  });
+
+  useEffect(() => {
+    const getLoginStatus = async () => {
+      const res = await fetch(`${baseUrl}/api/login/status`, {
+        method: "POST",
+        body: JSON.stringify({
+          token: localStorage.getItem("token") || "",
+        })
+      });
+      const data: LoginStatusResponse = await res.json();
+      setLoginStatus({
+        status: data.success,
+        username: data.data.username,
+      });
+    }
+    if (localStorage.getItem("token")) {
+      getLoginStatus();
+    } else {
+      setLoginStatus({
+        status: false,
+        username: "",
+      });
+    }
+  }, [])
+
   return (
     <ChakraProvider>
-      <Header onOpen={props.onOpen} />
-      <LoginModal {...props} />
-      <Component {...pageProps} />
+      <LoginStatusContext.Provider value={{ loginStatus, setLoginStatus }}>
+        <Header onOpen={props.onOpen} />
+        <LoginModal {...props} />
+        <Component {...pageProps} />
+      </LoginStatusContext.Provider>
     </ChakraProvider>
   );
 }

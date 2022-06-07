152c766b80cb0652cd77009e57c01dc149dc3d82
tohsaka888 <156132264@qq.com>
2022-06-07 16:53:45 +0800

    style: 更新样式

--- markdown/Paragraph.tsx
+++ markdown/Paragraph.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text } from "@chakra-ui/react";
 
 function Paragraph(props: any) {
-  return <Text margin={"8px 0px"} {...props} ml={"4px"} />;
+  return <Text margin={"8px 24px"} {...props} ml={"4px"} />;
 }
 
 export default Paragraph;

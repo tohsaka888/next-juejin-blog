08d79d6934551289be6bd9270d749bec7b2ef284
tohsaka888 <156132264@qq.com>
2022-06-07 13:23:50 +0800

    feat: 增加图床域名

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-import removeImport from 'next-remove-imports';
+import removeImport from "next-remove-imports";
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['p3-juejin.byteimg.com', 'p6-juejin.byteimg.com'],
+    domains: ["p3-juejin.byteimg.com", "p6-juejin.byteimg.com", "s1.aigei.com"],
   },
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-}
-
-
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+};
 
 export default removeImport()(nextConfig);

d8ae9f5fcb43e85f5bcfc6baab860bf14fe24794
tohsaka888 <156132264@qq.com>
2022-06-07 13:25:57 +0800

    feat: 请求列表

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,27 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectDB from "lib/connectDb";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ArticleBriefInfo, ListResponse } from "../../config/type";
+import { ListResponse } from "../../config/type";
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ListResponse>
 ) {
-  const list: ArticleBriefInfo[] = [];
-  for (let i = 0; i < 100; i++) {
-    list.push({
-      id: i,
-      title: `文章标题${i}--React`,
-      tags: ["React", "Next.js"],
-      coverImage:
-        "https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/92273c48d6e04babadae9969efa6d4b9~tplv-k3u1fbpfcp-zoom-crop-mark:1304:1304:1304:734.awebp??",
-      views: i * 100,
-      like: i * 100,
-      comments: i * 100,
-      date: "2020-01-01",
-      author: "tohsaka888",
-      intro:
-        "四处运行，即跨端技术的真谛。android、ios、pc、小程序，甚至智能手表、车载电视等场景下，需要应用到跨端技术实在是太多了。此次我们的主题围绕跨端技术详细展开~",
-    });
-  }
+  const body = JSON.parse(req.body || {});
+  const page: number = body.page - 1;
+  const limit: number = body.limit;
+  const db = await connectDB();
+  const listCollection = await db.collection("list");
+  const list = await listCollection.find({}).sort({ date: -1 }).skip(page * limit).limit(limit).toArray();
   res.status(200).json({ success: true, list: list });
 }

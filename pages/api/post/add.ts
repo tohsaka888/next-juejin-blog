fa049e577ed6a60d825f439f58029378acbeaa4f
tohsaka888 <156132264@qq.com>
2022-06-07 13:26:21 +0800

    feat: 新增文章

---
+++ pages/api/post/add.ts
@@ -0,0 +1,63 @@
+import { AddResponse } from 'config/type';
+import connectDB from 'lib/connectDb';
+import moment from 'moment';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler
+  (req: NextApiRequest, res: NextApiResponse<AddResponse>) {
+  try {
+    const body = JSON.parse(req.body || {});
+    const coverImage = body.coverImage;
+    const title = body.title;
+    const content = body.content;
+    const tags = body.tags;
+    const author = body.author;
+    const avatarUrl = "https://pic3.zhimg.com/80/v2-e987c60f8776df32c010265d2b81526e_720w.jpg";
+    const date = new Date().getTime();
+    const intro = body.intro;
+    const views = 0;
+    const like = 0;
+    const comments = 0;
+
+    const db = await connectDB();
+    const articleCollection = db.collection('articles');
+    const result = await articleCollection.insertOne({
+      coverImage,
+      title,
+      content,
+      tags,
+      author,
+      avatarUrl,
+      date,
+      intro,
+      views,
+      like,
+      comments
+    });
+
+    const listCollection = await db.collection('list');
+    await listCollection.insertOne({
+      coverImage,
+      title,
+      id: result.insertedId,
+      author,
+      avatarUrl,
+      date,
+      intro,
+      views,
+      like,
+      comments,
+      tags
+    });
+    res.status(200).json({
+      success: true,
+      id: result.insertedId
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      msg: error.message,
+    });
+  }
+
+}

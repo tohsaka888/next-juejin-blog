bf010b3e42b8751a4019538069fa2bb4a9adf95c
tohsaka888 <156132264@qq.com>
2022-06-08 09:17:38 +0800

    feat: 支持图床

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,12 @@ import removeImport from "next-remove-imports";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["p3-juejin.byteimg.com", "p6-juejin.byteimg.com", "s1.aigei.com"],
+    domains: [
+      "p3-juejin.byteimg.com",
+      "p6-juejin.byteimg.com",
+      "s1.aigei.com",
+      "s1.ax1x.com"
+    ],
   },
   compiler: {
     // ssr and displayName are configured by default

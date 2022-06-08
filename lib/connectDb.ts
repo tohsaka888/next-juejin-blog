342cdc7572db3632b0bd2ac50d3dfe0a4434e6c6
tohsaka888 <156132264@qq.com>
2022-06-08 08:31:02 +0800

    feat: update

--- lib/connectDb.ts
+++ lib/connectDb.ts
@@ -3,7 +3,7 @@ import { MongoSessionDaemon } from 'mongo-client-daemon'
 // mongoDB连接组件
 const connectDB = async () => {
   const db = new MongoSessionDaemon(
-    "mongodb://124.223.196.92/"
+    "mongodb://sdd:225522@124.223.196.92:27017/?authMechanism=DEFAULT&authSource=JueJinBlog"
   ); //输入连接数据库的地址
   const getSession = await db.getSession();
   return await getSession.db("JueJinBlog");

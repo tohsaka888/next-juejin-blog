4bdcada34889757c5f0616962e5c45545a08dd48
tohsaka888 <156132264@qq.com>
2022-06-09 14:23:42 +0800

    feat: 发送评论之后获取评论列表

--- components/Comment/index.tsx
+++ components/Comment/index.tsx
@@ -1,13 +1,36 @@
 import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { baseUrl } from 'config/baseUrl'
+import { CommentList, CommentResponse } from 'config/type'
+import React, { useEffect, useState, useCallback } from 'react'
 import List from './List'
 import SendArea from './SendArea'
 
-function Comment() {
+function Comment({ id, comments }: { id: string, comments: CommentList[] }) {
+  const [commentList, setCommentList] = useState<CommentList[]>(comments)
+  const [isSend, setIsSend] = useState<boolean>(false)
+
+  const getCommentList = useCallback(async () => {
+    const res = await fetch(`${baseUrl}/api/comments/list`, {
+      method: 'POST',
+      body: JSON.stringify({
+        articleId: id
+      })
+    })
+    const data: CommentResponse = await res.json()
+    setCommentList(data.list)
+  }, [id])
+
+  useEffect(() => {
+    if (isSend) {
+      getCommentList()
+      setIsSend(false)
+    }
+  }, [isSend, getCommentList])
+
   return (
     <Box mt={"24px"}>
-      <SendArea />
-      <List />
+      <SendArea id={id} setIsSend={setIsSend} />
+      <List comments={commentList} />
     </Box>
   )
 }

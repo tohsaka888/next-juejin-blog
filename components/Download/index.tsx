4cc5344ef59be93b484bb520ab4759280cbef50e
tohsaka888 <156132264@qq.com>
2022-06-13 16:53:25 +0800

    fix: 修复build错误

--- components/Download/index.tsx
+++ components/Download/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Flex, useColorMode, Text } from "@chakra-ui/react";
 import { shadows } from "config/theme";
-import download from "../../assets/image/download.png";
 import React from "react";
 import Image from "next/image";
 
@@ -14,7 +13,7 @@ function Download() {
       mt={"16px"}
     >
       <Flex>
-        <Image src={download} alt={"download"} width={"50px"} height={"50px"} />
+        <Image src={'/download.png'} alt={"download"} width={"50px"} height={"50px"} />
         <Flex ml={"8px"} flexDir={"column"} justify={"space-between"}>
           <Text fontSize={"15px"} fontWeight={"500"}>
             下载稀土掘金App

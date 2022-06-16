6bf457941d7e05f35eb7c3f8169d4b411359a43c
tohsaka888 <156132264@qq.com>
2022-06-16 16:36:48 +0800

    feat: update

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ var https = require("https");
 var fs = require("fs");
 
 const next = require("next");
-const port = 3000;
+const port = 443;
 const dev = process.env.NODE_ENV !== "production";
 
 const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
 
 var options = {
-  key: fs.readFileSync("3_react-animation.cloud.key"),
-  cert: fs.readFileSync("2_react-animation.cloud.crt"),
-  ca: [fs.readFileSync("1_root_bundle.crt")],
+  key: fs.readFileSync("2_react-animation.cloud.key"),
+  cert: fs.readFileSync("1_react-animation.cloud_bundle.crt"),
 };
 
 app.prepare().then(() => {
-  https
-    .createServer(options, (req, res) => {})
-    .listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on localhost:${port}`);
-    });
+  const server = https.createServer(options, (req, res) => {
+    handle(req, res)
+  });
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on localhost:${port}`);
+  });
 });

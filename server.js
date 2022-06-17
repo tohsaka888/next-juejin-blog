63d03bc061413cb396b8898dfac2e55add513e1b
tohsaka888 <156132264@qq.com>
2022-06-17 09:39:13 +0800

    feat: 测试https部署

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var https = require("https");
 var fs = require("fs");
 
 const next = require("next");
-const port = 443;
+const { parse } = require("url");
+const port = parseInt(process.env.PORT, 10) || 443;
 const dev = process.env.NODE_ENV !== "production";
+const hostname = "react-animation.cloud";
 
-const app = next({ dev, dir: __dirname });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 var options = {
@@ -14,11 +16,13 @@ var options = {
 };
 
 app.prepare().then(() => {
-  const server = https.createServer(options, (req, res) => {
-    res.write("hello,world");
-  });
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on localhost:${port}`);
-  });
+  https
+    .createServer(options, (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    })
+    .listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on https://${hostname}:${port}`);
+    });
 });

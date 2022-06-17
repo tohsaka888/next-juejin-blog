526db837a5b311ef090c8bf56f346a12a38f9696
tohsaka888 <156132264@qq.com>
2022-06-17 09:57:49 +0800

    feat: update

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var fs = require("fs");
 
 const next = require("next");
 const { parse } = require("url");
-const port = parseInt(process.env.PORT, 10) || 443;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "react-animation.cloud";
+const hostname = "localhost";
 
 const app = next({ dev });
 const handle = app.getRequestHandler();

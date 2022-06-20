3747d0198f8bd56f782115b34c73b7d4483546ca
tohsaka888 <156132264@qq.com>
2022-06-20 09:13:20 +0800

    feat: 增加友情链接

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,16 +49,9 @@ const Home: NextPage<{ articleList: ArticleBriefInfo[] }> = ({ articleList }) =>
         className={styles.footer}
         style={{ boxShadow: shadows[colorMode] }}
       >
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <Flex justify={"center"}>
+          友情链接: <a href={"https://www.baidu.com/"} style={{ color: '#1890ff', marginLeft: '16px' }}>海黄商城</a>
+        </Flex>
       </footer>
     </div>
   );

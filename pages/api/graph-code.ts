6b9726f3f6f3d1de099f7982276cc10f533ea574
tohsaka888 <156132264@qq.com>
2022-06-23 09:35:04 +0800

    feat: 生成图形验证码api

---
+++ pages/api/graph-code.ts
@@ -0,0 +1,32 @@
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import svgCaptcha, { CaptchaObj } from "svg-captcha";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<CaptchaObj>
+) {
+  let result: CaptchaObj = null!
+  if (Math.random() > 0.5) {
+    result = svgCaptcha.create({
+      size: 4,
+      noise: 3,
+      color: true,
+      background: '#cc9966',
+      width: 100,
+      height: 38
+    })
+  } else {
+    result = svgCaptcha.createMathExpr({
+      noise: 3,
+      color: true,
+      background: '#cc9966',
+      width: 100,
+      height: 38
+    })
+  }
+  console.log(result)
+
+  res.status(200).json({ ...result });
+}

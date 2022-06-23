088b6c8665bc3ec087e92f459503f7591025d862
tohsaka888 <156132264@qq.com>
2022-06-23 08:28:41 +0800

    feat: update

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -2,3 +2,5 @@ export const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://www.react-animation.cloud/"
     : "http://localhost:3000";
+
+export const commonUrl = `https://www.react-animation.cloud:9306/`

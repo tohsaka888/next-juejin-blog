a26426ea22356c14e1f41d65ef6ed2eda58de45c
tohsaka888 <156132264@qq.com>
2022-06-23 10:01:52 +0800

    feat: 国外线路,测试环境

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://www.react-animation.cloud/"
+    ? "https://next-juejin-blog.vercel.app"
     : "http://localhost:3000";
